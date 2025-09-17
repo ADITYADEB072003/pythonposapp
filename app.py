c8a41c7d6ecf2f01634e1d1b2f9a50808f468233
amzonclone <adityj81@gmail.com>
2025-09-18 01:08:54 +0530

    navabarand sales fixed

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def sales_chart():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
 
 
